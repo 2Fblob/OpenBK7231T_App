e6f54f6e70700e511ee428b375805b3ca61d0801
2Fblob <164585100+2Fblob@users.noreply.github.com>
2025-03-01 00:59:34 +0100

    Update drv_bl_shared.c

--- src/driver/drv_bl_shared.c
+++ src/driver/drv_bl_shared.c
@@ -354,9 +354,10 @@ void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
 		// -------------------------------------------
 		// Calculate the energy deficit by subtracting current energy from previous energy
 		int energy_deficit = estimated_energy_hour - charger_c_previous_energy;
+		//int scaled_deficit = 0;
 		
 		// Scale the energy deficit to the range [0, 100]
-		scaled_deficit = (energy_deficit + 50) / 10;  // Scale the deficit
+		int scaled_deficit = (energy_deficit + 50) / 10;  // Scale the deficit
 		scaled_deficit = (scaled_deficit < 0) ? 0 : (scaled_deficit > 100) ? 100 : scaled_deficit;  // Clamp the value between 0 and 100
 		
 		// Save the scaled deficit value to the output variable
@@ -978,7 +979,7 @@ void BL_ProcessUpdate(float voltage, float current, float power,
 				        {
 				            ip_middle = "/cm?cmnd=Dimmer3%20";  // Use Dimmer3 command if the IP is 20
 					    	// Save the last value that was sent to the charger			
-						charger_c_previous_energy = dump_load_relay[5]
+						charger_c_previous_energy = dump_load_relay[5];
 				        }
 				
 				        // Format the full command
