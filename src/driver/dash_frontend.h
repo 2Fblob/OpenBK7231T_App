037aa2b33cf1a0e3e9dd5d0da48e8dfccd36caf4
2Fblob <164585100+2Fblob@users.noreply.github.com>
2026-06-14 17:13:57 +0100

    Update dash_frontend.h

--- src/driver/dash_frontend.h
+++ src/driver/dash_frontend.h
@@ -3,7 +3,7 @@
 
 // Make sure to include the header that defines http_request_t in your project
 // (This is usually http_server.h or drv_http.h in OpenBeken)
-#include "http_server.h" 
+#include "../httpserver/new_http.h"
 
 void send_dash_javascript(http_request_t *request);
 
